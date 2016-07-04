f42b8df6569b83cf953c269c7e74c4ccbec5612d
baglesonali21 <baglesonali21@gmail.com>
2016-07-03 22:11:13 -0700

    Create Contact.php
    Contact details.

---
+++ Contact.php
@@ -0,0 +1,21 @@
+<html>
+	<title>About</title>
+	<?php include "header.php" ?>
+	<body>
+	<h1 align="center" style="color:">Contact Us</h1>
+	<p align="center" style="color:dodgerblue">
+	<strong><i>Sonali Bagle</i></strong><br>
+	<br></p>
+
+	
+	<br>
+	<hr width="80%">
+	<p align="center" style="color:dodgerblue"></style><br><br>	<strong>
+	Sonali Bagle:9028306930</strong><br>Email:baglesonali21@gmail.com<br><br><strong>
+	
+	<br>
+	<hr width="80%">
+	<p align="center" style="color:dodgerblue"><strong>Twitter</strong><br>Visit:www.twitter.com</p>
+	<?php include "footer.php" ?>
+	</body>
+</html>
