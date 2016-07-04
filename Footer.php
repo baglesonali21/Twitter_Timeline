c9d35b05d406ff67a418165768f9705b30bb97cb
baglesonali21 <baglesonali21@gmail.com>
2016-07-03 22:12:32 -0700

    Create Footer.php
    Footer provided the application.

---
+++ Footer.php
@@ -0,0 +1,17 @@
+
+<html>
+<style>
+	#div4{
+	border:1px solid;
+	height:75px;
+     margin-top:350px;
+	background-color:#1E90FF;
+     }
+
+</style>	  
+<body>
+	<center>
+		<div id="div4">All Rights Reserved &copy Twitter<br> Developed by: Twitter</div>
+	 </center> 
+</body>
+</html>
